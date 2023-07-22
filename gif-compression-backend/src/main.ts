748a79743eb9a5af0c8697f95b9d2063de5189d3
yunumu <1416900346@qq.com>
2023-07-22 23:36:14 +0800

    sharp img

---
+++ gif-compression-backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
+
+  await app.listen(3005);
+}
+bootstrap();
